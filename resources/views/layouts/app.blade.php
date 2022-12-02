1b5413692b7373df30a03a3b92aec6c0c2ed2829
Imam Fahmi F <63537236+imamdev93@users.noreply.github.com>
2022-12-02 18:42:07 +0700

    responsive mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,11 +72,6 @@
             <div class="container">
                 <div class="row align-items-center justify-content-between">
                     <div class="col">
-                        <div class="mobile-logo">
-                            <a href="index.html"><img src="assets/images/logo/logo.png" alt=""></a>
-                        </div>
-                    </div>
-                    <div class="col">
                         <div class="mobile-action-link text-end">
                             <a href="#mobile-menu-offcanvas" class="offcanvas-toggle offside-menu"><i
                                     class="icofont-navigation-menu"></i></a>
@@ -100,53 +95,12 @@
                     <!-- Start Mobile Menu Nav -->
                     <div class="offcanvas-menu">
                         <ul>
-                            <li><a href="index.html"><span>Home</span></a></li>
-                            <li><a href="about.html"><span>About Us</span></a></li>
-                            <li>
-                                <a href="#"><span>Services</span></a>
-                                <ul class="mobile-sub-menu">
-                                    <li><a href="service-list.html">Service List</a></li>
-                                    <li><a href="service-details.html">Service Details</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#"><span>Pages</span></a>
-                                <ul class="mobile-sub-menu">
-                                    <li><a href="case-list.html">Cases</a></li>
-                                    <li><a href="cases-details.html">Cases Details</a></li>
-                                    <li><a href="faq.html">FAQ</a></li>
-                                    <li><a href="404-error.html">404 Page</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#"><span>Blog</span></a>
-                                <ul class="mobile-sub-menu">
-                                    <li><a href="blog-list.html">Blog List Full Width</a></li>
-                                    <li><a href="blog-list-left-sidebar.html">Blog List Left Sidebar</a></li>
-                                    <li><a href="blog-list-right-sidebar.html">Blog List Right Sidebar</a></li>
-                                    <li><a href="blog-details.html">Blog Details Full Width</a></li>
-                                    <li><a href="blog-details-left-sidebar.html">Blog Details Left Sidebar</a></li>
-                                    <li><a href="blog-details-right-sidebar.html">Blog Details Right Sidebar</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="contact.html"><span>Contact</span></a>
-                            </li>
+                            <li><a href="/"><span>Beranda</span></a></li>
+                            <li><a href="/dashboard"><span>Monitoring</span></a></li>
+                            <li><a href="/login"><span>Login</span></a></li>
                         </ul>
                     </div> <!-- End Mobile Menu Nav -->
                 </div> <!-- End Mobile Menu -->
-
-                <!-- Start Mobile contact Info -->
-                <div class="mobile-contact-info text-center">
-                    <ul class="social-link">
-                        <li><a target="_blank" href="https://example.com"><i class="icofont-facebook"></i></a>
-                        </li>
-                        <li><a target="_blank" href="https://example.com"><i class="icofont-twitter"></i></a>
-                        </li>
-                        <li><a target="_blank" href="https://example.com"><i class="icofont-skype"></i></a></li>
-                    </ul>
-                </div>
-                <!-- End Mobile contact Info -->
 
             </div> <!-- End Offcanvas Mobile Menu Wrapper -->
         </div>
